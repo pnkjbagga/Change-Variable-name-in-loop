9dbcdec7574b4c0deec2a6375088f885f20f291b
pnkjbagga <pnkjbagga@gmail.com>
2016-04-15 23:08:18 +0200

    VARIABLE AS A LOOP
    Change Variable Name also as a Loop

---
+++ change_variable_name_in_loop.m
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                      %
+%                    Best Calculator in the World                      %
+%                                                                      %
+%                     By :- Er. Pankaj Bagga                           %
+%                          email : pnkjbagga@live.in                   %
+%                          www.pankajbagga.com                         %
+%                          +91 9779363902                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                      %
+%                     E2MATRIX Research LAB                            %
+%                                                                      %
+%                    Email   : support@e2matrix.com                    %
+%                    Website : www.e2matrix.com                        %
+%                    Blog    : www.e2matrix.com/blog                   %
+%                    Tel     : +91 9041262727                          %
+%                                                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+msgbox('E2MATRIX Research LAB')
+for i=1:10
+eval(['A_' num2str(i) '=i'])
+pause(1);
+end
+ msgbox('Download more codes at www.e2matrix.com/blog')
